be7fb1c8373b8d347e83136459101b95b90973f3
sin10490 <sin10490@sheridancollege.ca>
2020-06-05 10:10:21 -0400

    second commit

--- src/w1fndmtl/W1Fndmtl.java
+++ src/w1fndmtl/W1Fndmtl.java
@@ -18,24 +18,7 @@ public class W1Fndmtl {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-         char[] pillBox1=new char[7];
-        
-      Scanner sc=new Scanner(System.in);
-        
-        System.out.println("Please Enter a Word");
-        String word=sc.nextLine();
-        
-        char[] c=new char[word.length()];
-        
-        for(int i=0;i<word.length();i++)
-        {
-            c[i]=word.charAt(i);
-        }
-     //write loop to print the characters in reverse
-     for(int i=c.length-1;i>=0;i--)
-     {
-         System.out.println(c[i]);
-     }
+         System.out.println("Learning push");
     }
     
 }
